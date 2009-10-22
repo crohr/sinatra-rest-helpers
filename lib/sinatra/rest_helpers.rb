ef00ac69b7eb1f47ad73d9dc5a5391c90183918d
Cyril Rohr <cyril.rohr@gmail.com>
2009-10-22 09:23:51 +0200

    added etag computation helper

--- lib/sinatra/rest_helpers.rb
+++ lib/sinatra/rest_helpers.rb
@@ -1,4 +1,5 @@
 require 'rack'
+require 'digest/sha1'
 
 module Sinatra
   # 
@@ -43,6 +44,26 @@ module Sinatra
         response.headers['Content-Type'] = "#{selected_format["type"]}#{selected_format["level"].nil? ? "" : ";level=#{selected_format["level"]}"}"
       end
     end
+    
+    # parser_selector must respond to :select(content_type) and return a parser object with a :load method.
+    def parse_input_data!(parser_selector, options = {:limit => 10*1024})
+      case (mime_type = request.env['CONTENT_TYPE'])
+      when nil
+        halt 400, "You must provide a Content-Type HTTP header."
+      when /application\/x-www-form-urlencoded/i
+        request.env['rack.request.form_hash']
+      else
+        input_data = request.env['rack.input'].read
+        halt 400, "Input data size must not be empty and must not exceed #{options[:limit]} bytes." if (options[:limit] && input_data.length > options[:limit]) || input_data.length == 0
+        parser_selector.select(mime_type).load(input_data)
+      end
+    end
+    
+    def compute_etag(*args)  # :nodoc:
+      raise ArgumentError, "You must provide at least one parameter for the ETag computation" if args.empty?
+      Digest::SHA1.hexdigest(args.join("."))
+    end
+    
   end
-
+  
 end
--- spec/sinatra_rest_helpers_spec.rb
+++ spec/sinatra_rest_helpers_spec.rb
@@ -13,6 +13,11 @@ class App
   def test_provides!(*params)
     provides *params
   end
+  
+  def test_parse_input_data!(*params)
+    parse_input_data!(*params)
+  end
+  
 end
 
 def mime(ext, type)
@@ -24,69 +29,119 @@ describe "SinatraRestHelpers" do
   before(:each) do
     Rack::Mime::MIME_TYPES.clear
   end
-  it "should throw a 406 if the client requested only unsupported formats" do
-    request = mock("request", :accept => ["application/json"])
-    app = App.new(request)
-    app.should_receive(:halt).with(406, "")
-    app.test_provides!(:json)
+  describe ":provides" do
+    it "should throw a 406 if the client requested only unsupported formats" do
+      request = mock("request", :accept => ["application/json"])
+      app = App.new(request)
+      app.should_receive(:halt).with(406, "")
+      app.test_provides!(:json)
+    end
+    it "should not throw a 406 if the client requested a supported format" do
+      request = mock("request", :accept => ["application/json"])
+      mime :json, "application/json"
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!(:json)
+      app.response.headers['Content-Type'].should == "application/json"
+    end
+    it "should be case insensitive" do
+      request = mock("request", :accept => ["application/json"])
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!("application/JSON")
+      app.response.headers['Content-Type'].should == "application/JSON"
+    end
+    it "should not accept a request with a type level lower than what is supported" do
+      request = mock("request", :accept => ["application/json;level=1"])
+      app = App.new(request)
+      app.should_receive(:halt).with(406, "application/json;level=3, application/json;level=2")
+      app.test_provides!("application/json;level=3", "application/json;level=2")
+    end
+    it "should accept a request having a supported mime type, but with no level" do
+      request = mock("request", :accept => ["application/json"])
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!("application/json;level=2")
+      app.response.headers['Content-Type'].should == "application/json;level=2"
+    end
+    it "should select the first type matching the criteria" do
+      request = mock("request", :accept => ["application/json;level=2", "application/xml", "application/vnd.fr.grid5000.api.Cluster+json;level=2"])
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!("application/json;level=3", "application/vnd.fr.grid5000.api.Cluster+json;level=2")
+      app.response.headers['Content-Type'].should == "application/vnd.fr.grid5000.api.Cluster+json;level=2"
+    end
+    it "should accept requests with a level, even if the developer didn't explicitely defined one" do
+      request = mock("request", :accept => ["application/json;level=1"])
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!("application/json")
+      app.response.headers['Content-Type'].should == "application/json"
+    end
+    it "should correctly deal with widlcard characters [client-side, I]" do
+      request = mock("request", :accept => ["application/vnd.fr.grid5000.api.*+json"])
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!("application/vnd.fr.grid5000.api.Cluster+json;level=1")
+      app.response.headers['Content-Type'].should == "application/vnd.fr.grid5000.api.Cluster+json;level=1"
+    end
+    it "should correctly deal with widlcard characters [client-side, II]" do
+      request = mock("request", :accept => ["application/*"])
+      app = App.new(request)
+      app.should_not_receive(:halt)
+      app.test_provides!("application/vnd.fr.grid5000.api.Cluster+json;level=1")
+      app.response.headers['Content-Type'].should == "application/vnd.fr.grid5000.api.Cluster+json;level=1"
+    end
   end
-  it "should not throw a 406 if the client requested a supported format" do
-    request = mock("request", :accept => ["application/json"])
-    mime :json, "application/json"
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!(:json)
-    app.response.headers['Content-Type'].should == "application/json"
+  
+  describe ":parse_input_data!" do
+    it "should load input data in application/x-www-form-urlencoded format" do
+      parser_selector = mock("parser_selector")
+      request = mock("request", :env => {'rack.request.form_hash' => {"foo" => "bar"}, 'CONTENT_TYPE' => 'application/x-www-form-urlencoded'})
+      app = App.new(request)
+      parser_selector.should_not_receive(:select)
+      app.test_parse_input_data!(parser_selector).should == {"foo" => "bar"}
+    end
+    it "should load input data in application/json format" do
+      require 'json'
+      parser_selector = mock("parser_selector")
+      request = mock("request", :env => {'rack.input' => StringIO.new({"foo" => "bar"}.to_json), 'CONTENT_TYPE' => 'application/json'})
+      app = App.new(request)
+      parser_selector.should_receive(:select).with('application/json').and_return(JSON)
+      app.test_parse_input_data!(parser_selector).should == {"foo" => "bar"}
+    end
+    it "should halt with a 400 status code if the input data is empty" do
+      parser_selector = mock("parser_selector")
+      request = mock("request", :env => {'rack.input' => StringIO.new(), 'CONTENT_TYPE' => 'application/json'})
+      app = App.new(request)
+      app.should_receive(:halt).with(400, /must not be empty/).and_raise(Exception.new)
+      lambda{app.test_parse_input_data!(parser_selector)}.should raise_error(Exception)
+    end
+    it "should halt with a 400 status code if the input data is over the limit" do
+      parser_selector = mock("parser_selector")
+      request = mock("request", :env => {'rack.input' => StringIO.new("123456789_1"), 'CONTENT_TYPE' => 'application/json'})
+      app = App.new(request)
+      app.should_receive(:halt).with(400, /must not exceed 10 bytes/).and_raise(Exception.new)
+      lambda{app.test_parse_input_data!(parser_selector, :limit => 10)}.should raise_error(Exception)
+    end
+    it "should halt with a 400 status code if the content type is not set" do
+      parser_selector = mock("parser_selector")
+      request = mock("request", :env => {'rack.input' => StringIO.new("123456789_1")})
+      app = App.new(request)
+      app.should_receive(:halt).with(400, /must provide a Content-Type HTTP header/)
+      app.test_parse_input_data!(parser_selector)
+    end
   end
-  it "should be case insensitive" do
-    request = mock("request", :accept => ["application/json"])
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!("application/JSON")
-    app.response.headers['Content-Type'].should == "application/JSON"
-  end
-  it "should not accept a request with a type level lower than what is supported" do
-    request = mock("request", :accept => ["application/json;level=1"])
-    app = App.new(request)
-    app.should_receive(:halt).with(406, "application/json;level=3, application/json;level=2")
-    app.test_provides!("application/json;level=3", "application/json;level=2")
-  end
-  it "should accept a request having a supported mime type, but with no level" do
-    request = mock("request", :accept => ["application/json"])
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!("application/json;level=2")
-    app.response.headers['Content-Type'].should == "application/json;level=2"
-  end
-  it "should select the first type matching the criteria" do
-    request = mock("request", :accept => ["application/json;level=2", "application/xml", "application/vnd.fr.grid5000.api.Cluster+json;level=2"])
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!("application/json;level=3", "application/vnd.fr.grid5000.api.Cluster+json;level=2")
-    app.response.headers['Content-Type'].should == "application/vnd.fr.grid5000.api.Cluster+json;level=2"
-  end
-  it "should accept requests with a level, even if the developer didn't explicitely defined one" do
-    request = mock("request", :accept => ["application/json;level=1"])
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!("application/json")
-    app.response.headers['Content-Type'].should == "application/json"
-  end
-  it "should correctly deal with widlcard characters [client-side, I]" do
-    request = mock("request", :accept => ["application/vnd.fr.grid5000.api.*+json"])
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!("application/vnd.fr.grid5000.api.Cluster+json;level=1")
-    app.response.headers['Content-Type'].should == "application/vnd.fr.grid5000.api.Cluster+json;level=1"
-  end
-  it "should correctly deal with widlcard characters [client-side, II]" do
-    request = mock("request", :accept => ["application/*"])
-    app = App.new(request)
-    app.should_not_receive(:halt)
-    app.test_provides!("application/vnd.fr.grid5000.api.Cluster+json;level=1")
-    app.response.headers['Content-Type'].should == "application/vnd.fr.grid5000.api.Cluster+json;level=1"
-  end
-  it "should description" do
-    
+  
+  describe ":compute_etag" do
+    it "should correctly compute the etag with multiple parameters" do
+      App.new(nil).compute_etag([1,2,3], 2, "whatever", :xyz).should == Digest::SHA1.hexdigest([[1,2,3], 2, "whatever", :xyz].join("."))
+    end
+    it "should correctly compute the etag with one parameter" do
+      App.new(nil).compute_etag(1).should == Digest::SHA1.hexdigest([1].join("."))
+    end
+    it "should raise an Argument Error if no arguments are present" do
+      lambda{App.new(nil).compute_etag()}.should raise_error(ArgumentError, /must provide at least one parameter/)
+    end
   end
 end
